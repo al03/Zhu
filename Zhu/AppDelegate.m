b73d369a6bfc463dfa1f2e12fae2e19036bc215c
al03 <maokaiyin@gmail.com>
2014-06-17 18:01:46 +0800

    tabbar

--- Zhu/AppDelegate.m
+++ Zhu/AppDelegate.m
@@ -13,6 +13,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    [[UINavigationBar appearance] setBackgroundImage:[UIImage imageNamed:@"navbarBg"] forBarMetrics:UIBarMetricsDefault];
+    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObject:[UIColor whiteColor] forKey:NSForegroundColorAttributeName]];
+    
+    [[UITabBar appearance] setTintColor:[UIColor whiteColor]];
     return YES;
 }
 							
